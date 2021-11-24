f99491e47290671dfaa56d9b80707f77e1845d9f
Yesid Varela <yleandrovarela@gmail.com>
2021-11-23 19:39:01 -0500

    Add list task styles and update function

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCheckSquare,
@@ -7,18 +7,47 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Task = ({ tarea }) => {
+  //State para editar una tarea
+  const [editartarea, SetEditarTarea] = useState(false);
+
+  //State para el form de editar
+  const [nuevaTarea, setNuevaTarea] = useState(tarea.texto);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    SetEditarTarea(false);
+  };
+
   return (
     <li className="lista-tareas__tarea">
       <FontAwesomeIcon
         icon={faCheckSquare}
         className="lista-tareas__icono lista-tareas__icono-check"
       />
-      <div className="lista-tareas__texto">{tarea.texto}</div>
+      <div className="lista-tareas__texto">
+        {/* Condicional para actualizar */}
+        {editartarea ? (
+          <form className="formulario-editar-tarea" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="formulario-editar-tarea__input"
+              value={nuevaTarea}
+              onChange={(e) => setNuevaTarea(e.target.value)}
+            />
+            <button type="submit" className="formulario-editar-tarea__btn">
+              Update
+            </button>
+          </form>
+        ) : (
+          tarea.texto
+        )}
+      </div>
 
       <div className="lista-tareas__contenedor-botones">
         <FontAwesomeIcon
           icon={faEdit}
           className="lista-tareas__icono lista-tareas__icono-accion"
+          onClick={() => SetEditarTarea(!editartarea)}
         />
 
         <FontAwesomeIcon
